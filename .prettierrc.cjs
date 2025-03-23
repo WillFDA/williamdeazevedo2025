27fed011baefe94a32eeb5ebe5f87c13f810b72b
William <william.deazevedopro@gmail.com>
2025-03-23 17:53:30 +0100

    avancement hoempage

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+};
