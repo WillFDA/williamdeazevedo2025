b0ae6ab78f06a0f77ecfe0a5a23003687666c3a0
William <william.deazevedopro@gmail.com>
2025-07-04 19:02:10 +0200

    feat(ui): rework on ui portfolio part1

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,31 @@
-import { MoveRight } from 'lucide-react';
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <>
-      <section>
-        <span>Joseph Joubert</span>
-        <h1>&quot;Transmettre, c&apos;est apprendre deux fois&quot;</h1>
-        <Link href="/about">En savoir plus</Link>
-      </section>
-      <section className="flex gap-16">
-        <div className="flex flex-col gap-4">
-          <h2>Derniers posts</h2>
-          <div className="flex flex-col gap-1">
-            <span>Oct 24</span>
-            <span className="font-bold text-2xl">
-              Comment j&apos;ai construit mon blog
+    <main className="max-w-7xl mx-auto flex flex-col p-4">
+      <header className="my-5 flex justify-between items-center mx-auto p-4 border border-gay-100"></header>
+      <div className=" border-gray-200 min-h-screen absolute custom-position-left"></div>
+      <div className=" border-gray-200 min-h-screen absolute custom-position-right"></div>
+      <section className="max-w-4xl mx-auto p-4  w-full mt-20">
+        <div className="flex flex-col mx-auto max-w-lg">
+          <div className="size-20 bg-gray-200 rounded-full mb-4"></div>
+          <h1 className="text-4xl font-bold mb-3">
+            Hello moi c&apos;est William <br />
+            Développeur Front end junior
+          </h1>
+          <div className="flex gap-2 items-center mb-4">
+            <div className="size-3 bg-green-400 rounded-full"></div>
+            <span className="text-gray-500">
+              Actuellement à la recherche d&apos;un CDI
             </span>
-            <p className="text-sm text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              quos.
-            </p>
-            <Link href="/post/1">Lire plus</Link>
           </div>
+          <button className="bg-black hover:bg-black/85 cursor-pointer text-white px-8 py-4 w-full rounded-lg transition-all duration-150 ease-in-out">
+            Voir mon CV
+          </button>
         </div>
-        <div className="flex flex-col gap-4">
-          <h3 className="text-sm font-bold">Catégories</h3>
-          <div className="flex flex-col gap-1">
-            <Link href="/category/1">JavaScript</Link>
-            <Link href="/category/2">React</Link>
-            <Link href="/category/3">Next.js</Link>
-          </div>
-          <h3 className="text-sm font-bold mt-4">Posts populaires</h3>
-          <div className="flex gap-2 items-center">
-            <MoveRight />
-            <span className="font-bold">La différence entre flex et grid</span>
-          </div>
+        <div className="relative mt-10 z-10 bg-red-300 w-full p-4 h-80">
+          <div className="absolute bg-gray-400 rounded-xl w-60 h-58 -rotate-5  left-0 z-1 top-10"></div>
+          <div className="absolute bg-gray-200 rounded-xl w-60 h-75 rotate-10 left-50 z-0"></div>
         </div>
       </section>
-    </>
+    </main>
   );
 }
