47eb5fdb9ed2168fae1dfc7db9671bdacc3a7e28
William <william.deazevedopro@gmail.com>
2025-08-17 23:49:55 +0200

    destruction total

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
-import Footer from '@/components/footer';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Navbar from '../components/navbar';
 import './globals.css';
 
 const geistSans = Geist({
@@ -16,33 +14,6 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: 'William De Azevedo',
   description: 'Développeur Front-end React / NextJS',
-  openGraph: {
-    title: 'William De Azevedo - Portfolio',
-    description:
-      'Développeur Front-end React / NextJS | Explorez mes projets et compétences',
-    url: 'https://williamdeazevedo.fr/',
-    siteName: 'Portfolio de William De Azevedo',
-    images: [
-      {
-        url: 'https://williamdeazevedo.fr/pictures/william.png',
-        width: 1200,
-        height: 630,
-        alt: 'William De Azevedo - Développeur Front-end',
-      },
-    ],
-    locale: 'fr_FR',
-    type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'William De Azevedo - Développeur Front-end',
-    description: 'Explorez mon portfolio de développeur React / NextJS',
-    images: ['https://williamdeazevedo.fr/pictures/william.png'],
-    creator: '@Williamdazevedo',
-  },
-  other: {
-    'linkedin:author': 'https://www.linkedin.com/in/william-de-azevedo/',
-  },
 };
 
 export default function RootLayout({
@@ -55,9 +26,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white min-h-screen overflow-x-hidden flex flex-col`}
       >
-        <Navbar />
         <main className="grow">{children}</main>
-        <Footer />
       </body>
     </html>
   );
